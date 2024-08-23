104f6d4267f4645a4816ba59b290d15cedffc9e1
GamingDriver <yotov180@gmail.com>
2024-08-23 15:07:34 +0300

    latest release of my earliest code

--- main/blink_example_main.c
+++ main/blink_example_main.c
@@ -14,13 +14,15 @@
 #include "esp_log.h"
 #include "led_strip.h"
 #include "sdkconfig.h"
+#include "driver/ledc.h"
 
 static const char *TAG = "example";
+
 
 /* Use project configuration menu (idf.py menuconfig) to choose the GPIO to blink,
    or you can edit the following line and set a number here.
 */
-#define BLINK_GPIO CONFIG_BLINK_GPIO
+#define BLINK_GPIO GPIO_NUM_2
 
 static uint8_t s_led_state = 0;
 
@@ -71,6 +73,57 @@ static void configure_led(void)
 
 #elif CONFIG_BLINK_LED_GPIO
 
+#define TAG "MOTOR_CONTROL"
+
+#define MOTOR1_PIN1 20
+#define MOTOR1_PIN2 21
+#define ENABLE1_PIN 14
+
+#define PWM_FREQUENCY 30000
+#define PWM_CHANNEL   LEDC_CHANNEL_0
+#define PWM_RESOLUTION LEDC_TIMER_8_BIT
+#define INITIAL_DUTY_CYCLE 200
+
+static int duty_cycle = INITIAL_DUTY_CYCLE;
+
+void configure_pwm() {
+    // Configure LEDC timer
+    ledc_timer_config_t ledc_timer = {
+        .speed_mode = LEDC_HIGH_SPEED_MODE,
+        .timer_num = LEDC_TIMER_0,
+        .duty_resolution = PWM_RESOLUTION,
+        .freq_hz = PWM_FREQUENCY,
+        .clk_cfg = LEDC_AUTO_CLK
+    };
+    ledc_timer_config(&ledc_timer);
+
+    // Configure LEDC channel
+    ledc_channel_config_t ledc_channel = {
+        .speed_mode = LEDC_HIGH_SPEED_MODE,
+        .channel = PWM_CHANNEL,
+        .timer_sel = LEDC_TIMER_0,
+        .intr_type = LEDC_INTR_DISABLE,
+        .gpio_num = ENABLE1_PIN,
+        .duty = 0,
+        .hpoint = 0
+    };
+    ledc_channel_config(&ledc_channel);
+}
+
+void configure_gpio() {
+    // Configure GPIO pins
+    gpio_pad_select_gpio(MOTOR1_PIN1);
+    gpio_set_direction(MOTOR1_PIN1, GPIO_MODE_OUTPUT);
+    
+    gpio_pad_select_gpio(MOTOR1_PIN2);
+    gpio_set_direction(MOTOR1_PIN2, GPIO_MODE_OUTPUT);
+
+    gpio_pad_select_gpio(ENABLE1_PIN);
+    gpio_set_direction(ENABLE1_PIN, GPIO_MODE_OUTPUT);
+}
+
+
+
 static void blink_led(void)
 {
     /* Set the GPIO level according to the state (LOW or HIGH)*/
@@ -91,8 +144,54 @@ static void configure_led(void)
 
 void app_main(void)
 {
+    configure_pwm();
+    configure_gpio();
 
-    uint16_t n = 125;
+    ESP_LOGI(TAG, "Testing DC Motor...");
+
+    while (true) {
+        // Move the DC motor forward at maximum speed
+        ESP_LOGI(TAG, "Moving Forward");
+        gpio_set_level(MOTOR1_PIN1, 0);
+        gpio_set_level(MOTOR1_PIN2, 1);
+        vTaskDelay(2000 / portTICK_PERIOD_MS);
+
+        // Stop the DC motor
+        ESP_LOGI(TAG, "Motor stopped");
+        gpio_set_level(MOTOR1_PIN1, 0);
+        gpio_set_level(MOTOR1_PIN2, 0);
+        vTaskDelay(1000 / portTICK_PERIOD_MS);
+        }
+        duty_cycle = INITIAL_DUTY_CYCLE;
+    }
+    /*
+    int led_state_user_input;
+    configure_led();
+    //uint16_t n = 125;
+    while(true){
+        scanf("%d", &led_state_user_input);
+        if (led_state_user_input == 1 && s_led_state == 0){
+            s_led_state = 1;
+            blink_led();
+            ESP_LOGI(TAG, "Turning the LED ON");
+        }
+        else if(led_state_user_input == 0 && s_led_state == 1){
+            s_led_state = 0;
+            blink_led();
+            ESP_LOGI(TAG, "Turning the LED OFF");
+        }
+    }
+    */
+        /*
+    while(true) {
+        ESP_LOGI(TAG, "Turning the LED %s! Period = %d", s_led_state == true ? "ON" : "OFF",n*2);
+        blink_led();
+        s_led_state = !s_led_state;
+        vTaskDelay(n / portTICK_PERIOD_MS);
+    }
+    */
+/*
+    
     bool x = false;
     configure_led();
 
@@ -113,5 +212,20 @@ void app_main(void)
         blink_led();
         s_led_state = !s_led_state;
         vTaskDelay(n / portTICK_PERIOD_MS);
+        //my_sum(n);
     }
+*/
+}
+
+//global s
+
+
+uint16_t my_sum(uint16_t n)
+{
+    uint16_t s = 0;
+    for (uint16_t i = 1; i <= 10; i++) //for i in range(1,10,1)
+    {
+        s = s + i;
+    }
+    return s;
 }
